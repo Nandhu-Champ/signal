250e6239c9c76dc0bf4dc7ed6c5b7283e28d960b
Nanduchamp <nandhu364cs@gmail.com>
2021-12-29 12:10:28 +0530

    no message

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-var a1 = 10;
-var b=20;
+
+var a = 10;
+var b=90;
+var c=9;
---
+++ index1 - Copy.js
@@ -0,0 +1,8 @@
+<<<<<<< Updated upstream
+var a1 = 10;
+var b=20;
+=======
+var a = 10;
+var b=90;
+var c=9;
+>>>>>>> Stashed changes
